83ea641f3c9202e9337e7856ce3eec22355a94b0
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-01-16 09:47:50 -0500

    update files

---
+++ emoji.py
@@ -0,0 +1,9 @@
+# Print emoji without any module
+
+# Method : Unicode CLDR NAME
+
+print("\N{thinking face}", end =" ")
+print("\N{winking face}", end =" ")
+print("\N{fire}", end =" ")
+print("\N{rocket}", end =" ")
+print("\N{family}", end =" ")
